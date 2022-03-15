48f28fc7c07e9e22d2e98886610a81f61eab3598
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-03-15 18:42:36 -0300

    correccion estettica

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ form.addEventListener('submit',actionSubmit);
 function actionSubmit(event) {
     event.preventDefault();
     const formulario = new FormData(this);
-    buttonEmail.setAttribute('href', `mailto:maxidelfino1234@hotmail.com?subject= Nombre: ${formulario.get('name')} Apellido: ${formulario.get('apellido')} Email: ${formulario.get('email')} Consulta: ${formulario.get('option')}&body=${formulario.get('consulta')}`);
+    buttonEmail.setAttribute('href', `mailto:maxidelfino1234@hotmail.com?subject= Nombre: ${formulario.get('name')} 
+    Apellido: ${formulario.get('apellido')}
+    Email: ${formulario.get('email')} 
+    Consulta: ${formulario.get('option')}&body=${formulario.get('consulta')}`);
     buttonEmail.click();
 }
