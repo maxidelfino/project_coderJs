578dfccda7e95ea3932f1f2ee48505333b863e84
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-03-15 18:23:43 -0300

    Vinculamos nuentro email al formulario para recibir las consultas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('#form');
+const buttonEmail = document.querySelector('#buttonEmail');
 form.addEventListener('submit',actionSubmit);
 
 function actionSubmit(event) {
-    // event.preventDefault();
+    event.preventDefault();
+    const formulario = new FormData(this);
+    buttonEmail.setAttribute('href', `mailto:maxidelfino1234@hotmail.com?subject= Nombre: ${formulario.get('name')} Apellido: ${formulario.get('apellido')} Email: ${formulario.get('email')} Consulta: ${formulario.get('option')}&body=${formulario.get('consulta')}`);
+    buttonEmail.click();
 }
